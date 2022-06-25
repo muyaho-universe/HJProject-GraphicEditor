dc46d8e1fa407dfdb01bb7e4df291ef0916a80a8
muyaho-universe <kimdaesk@gmail.com>
2022-06-25 11:43:25 +0900

    Add assets

---
+++ app/src/main/java/com/dale/graphiceditor/buttons/ColorChooserCallButton.java
@@ -0,0 +1,9 @@
+package com.dale.graphiceditor.buttons;
+
+import javax.swing.JButton;
+
+public class ColorChooserCallButton extends JButton {
+	public void createButton() {
+		
+	}
+}
--- app/src/main/java/com/dale/graphiceditor/panel/ColorEditPanel.java
+++ app/src/main/java/com/dale/graphiceditor/panel/ColorEditPanel.java
@@ -9,51 +9,15 @@ import javax.swing.event.ChangeListener;
 
 import com.dale.graphiceditor.GraphicEditorFrame;
 
-public class ColorEditPanel extends JPanel implements ChangeListener{
+public class ColorEditPanel extends JPanel {
 	public boolean hasMouse = false;
 	JColorChooser colorChooser = new JColorChooser();
 
 	public void createPanel() {
-		this.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				hasMouse = true;
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				hasMouse = false;
-			}
-			
-		});
+		
 	}
 	public void setSize() {
 		this.setBounds(540, 0, 70, GraphicEditorFrame.attributesPanelHeight);
 	}
-	@Override
-	public void stateChanged(ChangeEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
