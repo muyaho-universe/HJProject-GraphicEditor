7c0cd5ed3f1607e1c08dcf93715e3cde16b9885d
muyaho-universe <kimdaesk@gmail.com>
2022-06-26 16:18:07 +0900

    Create MyDatas class

---
+++ app/src/main/java/com/dale/graphiceditor/datapart/MyDatas.java
@@ -0,0 +1,13 @@
+package com.dale.graphiceditor.datapart;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class MyDatas {
+	public static ArrayList<Point> startVector = new ArrayList<Point>(); // 시작
+	public static ArrayList<Point> endVector = new ArrayList<Point>(); // 끝점
+	public static ArrayList<String> mode = new ArrayList<String>();//도형이름 저장
+	public static ArrayList<Color> color = new ArrayList<Color>();
+	public static ArrayList<Integer> stroke = new ArrayList<Integer>();
+
+}
