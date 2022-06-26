9c52c75b6afc37efd447bc1bc87d5449d2deee41
muyaho-universe <kimdaesk@gmail.com>
2022-06-26 16:24:07 +0900

    Implements several data structures

--- app/src/main/java/com/dale/graphiceditor/datapart/MyDatas.java
+++ app/src/main/java/com/dale/graphiceditor/datapart/MyDatas.java
@@ -2,6 +2,7 @@ package com.dale.graphiceditor.datapart;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class MyDatas {
 	public static ArrayList<Point> startVector = new ArrayList<Point>(); // 시작
@@ -9,5 +10,6 @@ public class MyDatas {
 	public static ArrayList<String> mode = new ArrayList<String>();//도형이름 저장
 	public static ArrayList<Color> color = new ArrayList<Color>();
 	public static ArrayList<Integer> stroke = new ArrayList<Integer>();
-
+	public static Stack<ArrayList<MyDatas>> forUndoStack = new Stack<ArrayList<MyDatas>>();
+	public static Stack<ArrayList<MyDatas>> forRedoStack = new Stack<ArrayList<MyDatas>>();
 }
